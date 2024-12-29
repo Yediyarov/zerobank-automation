a528217fb0b82f3b60a150c376cdf079e9baa8ba
Yediyarov <yediyarov.xeyal@gmail.com>
2024-12-29 15:43:03 +0400

    test: add test for searching a valid term

---
+++ tests/e2e/e2e-search.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test'
+import { HomePage } from '../../page-objects/HomePage'
+
+test.describe.parallel('Search Flow', () => {
+    let homePage: HomePage
+
+    // Before Hook
+    test.beforeEach(async ({page}) => {
+        homePage = new HomePage(page)
+        await homePage.visit()
+    })
+
+    // Search for a valid term
+    test('Search for a valid term', async ({page}) => {
+        const searchedPhase = 'bank'
+        await homePage.searchFor(searchedPhase)
+        await homePage.wait(3000)
+
+        const expectedText = `The following pages were found for the query: ${searchedPhase}`
+        await expect(page.getByText(expectedText)).toBeVisible();
+    })
+})
